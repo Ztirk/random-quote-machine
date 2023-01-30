eafc349365fb74070d43e06b0202dc6b59a280fa
Ztirk <krit15489@gmail.com>
2023-01-30 13:26:57 +0700

    goog

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-import React, {useState} from 'react';
-import './RandomQuoteMachine.css';
-
-export default function RandomQuoteMachine() {
+function RandomQuoteMachine() {
     const arr = ['"The purpose of our lives is to be happy." — Dalai Lama', 
     '"Life is what happens when you are busy making other plans." — John Lennon',
     '"Get busy living or get busy dying." — Stephen King',
@@ -9,9 +6,8 @@ export default function RandomQuoteMachine() {
     '"Many of life’s failures are people who did not realize how close they were to success when they gave up."– Thomas A. Edison'
 ];
     
-    const [quote, setQuote] = useState("");
-    const [bgColor, setbgColor] = useState("#fff");
-
+    const [quote, setQuote] = React.useState("Click the button below!");
+    const [bgColor, setbgColor] = React.useState("#fff");
     
     const randomClick = () => {
         const randomQuote = arr[Math.floor(Math.random()*arr.length)]
@@ -24,11 +20,14 @@ export default function RandomQuoteMachine() {
     }
 
     return (
-        <div style={{backgroundColor: `${bgColor}`}}>
-            <h1>Random-Quote-Machine</h1>
-            <h2>{quote}</h2>
-            <button type='button' onClick={() => {bgColorClick(); randomClick()}}>Try again</button>
+        <div class='d-flex justify-content-center align-items-center flex-column  vw-100 vh-100' style={{backgroundColor: `${bgColor}`}}>
+            <h1 class='text-center mb-5'>Random-Quote-Machine</h1>
+            <h2 class='bg-light rounded p-3'>{quote}</h2>
+            <div class='text-center mt-5'>
+                <button class='btn btn-primary' type='button' onClick={() => {bgColorClick(); randomClick()}}>Try again</button>
+            </div>
         </div>
     )
 }
 
+ReactDOM.render(<RandomQuoteMachine />, document.getElementById('root'));
